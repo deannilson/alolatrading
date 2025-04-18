74a26bf5c1b5ed4410995966135e2c00a11b3c5a
Funwi Che <funwiche.dev@gmail.com>
2025-04-18 13:51:27 +0100

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { APP_NAME } from "./utils";
 export default defineNuxtConfig({
-  ssr: false,
+  ssr: true,
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
   postcss: { plugins: { tailwindcss: {}, autoprefixer: {} } },
--- utils/index.ts
+++ utils/index.ts
@@ -1,8 +1,8 @@
 import locales from "../resources/locales.json";
 export const APP_NAME = "Alola Trading Ltd";
 export const APP_PHONE = "+66 2 2520452";
-export const APP_EMAIL = "info@alolatrading.com";
-export const APP_URL = "www.alolatrading.com";
+export const APP_EMAIL = "info@alolatradingltd.com";
+export const APP_URL = "www.alolatradingltd.com";
 export const APP_ADDRESS = `
 16th Fl., Office # 1602, Vanit Building 1<br />
 1126/1 New Petchburi Road, Makkasan, Ratchathewi<br />
